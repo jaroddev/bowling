d66a84545f34fc4ed3c26cf96d4d3e89294d6f95
dhya <otmanecherifdhya@gmail.com>
2021-10-08 15:03:51 +0200

    intercace

---
+++ bowling.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+def action():
+    # mettre un text par défaut pour l'entrée
+    entrée1.insert(0,11111)
+     
+    
+    # récupérer les données de l'entrée2 
+    score = int (entrée2.get())
+    print(score)
+
+
+# Création d'une fenêtre avec la classe Tk :
+fenetre = Tk()
+# Ajout d'un titre à la fenêtre principale :
+fenetre.title("My bowling")
+# Définir un icone :
+fenetre.iconbitmap("")
+# Définir les dimensions par défaut la fenêtre principale :
+fenetre.geometry("400x300")
+label1 = Label (fenetre, text = "Lancers")
+label1.place(x=50,y=50)
+
+entrée1 = Entry (fenetre)
+entrée1.place(x=200,y=50)
+
+label2 = Label (fenetre, text = "Score")
+label2.place(x=50,y=100)
+
+entrée2 = Entry (fenetre)
+entrée2.place(x=200,y=100)
+
+Calculer=Button(fenetre, text="Calculer", command = action)
+Calculer.place(x=200,y=150)
+# Affichage de la fenêtre créée :
+fenetre.mainloop()
